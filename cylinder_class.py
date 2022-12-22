8c08dcc2aca5e29a44feed91da19af442f5cacc9
ndjogo <n.djogo80@gmail.com>
2022-12-22 14:22:19 +0000

    Completed cylinder classes

---
+++ cylinder_class.py
@@ -0,0 +1,32 @@
+# We are going to define some classes 
+class Cylinder():
+    def __init__(self, height, radius = 1):
+        self.height = height 
+        self.radius = radius 
+        self.surface_area = None 
+        self.volume = None 
+
+
+    def get_surface_area(self):
+        #fucntion to get the surface area of a cylinder
+        pi = 22/7
+        self.surface_area = 2*(pi)*self.radius**2 + 2*(pi)*self.radius*self.height
+        return round(self.surface_area, 2)
+
+    def get_volume(self):
+        #function to get the area of a cylinder 
+        pi = 22/7 
+        self.volume = pi*(self.radius**2)*self.height
+        return round(self.volume, 2)
+
+
+if __name__ == "__main__":
+    cylinder_1 = Cylinder(2, 5)
+    print(cylinder_1.height, cylinder_1.radius, cylinder_1.surface_area, cylinder_1.volume)
+    print(cylinder_1.get_surface_area())
+    print(cylinder_1.get_volume())
+    print(cylinder_1.height, cylinder_1.radius, cylinder_1.surface_area, cylinder_1.volume)
+
+
+
+
