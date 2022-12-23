538015d84a25f6fbb70d194cd7d1e8576f90f9a3
ndjogo <n.djogo80@gmail.com>
2022-12-23 23:58:46 +0000

    Adding shape class

---
+++ shape_class.py
@@ -0,0 +1,46 @@
+#creating shape class 
+class Shape():
+    def __init__(self, num_sides, tesselates= False):
+        #raises error if incorrect number of sides is entered 
+        if num_sides <= 0:
+            raise Exception("Number of sides should be positive")
+        self.num_sides = num_sides 
+        self.tesselates = tesselates 
+    """   
+        def get_info(self):
+        #create a function to return shape info 
+            print(f"The number of sides is: {self.num_sides}")
+            if self.tesselates == True:
+                print("This shape tesselates!")
+            else: 
+                print("This shape does not tessealte!")
+    """
+
+    def get_info(self):
+        raise NotImplementedError
+
+#creating Circle class to inherit from Shape
+
+class Circle(Shape):
+    def __init__(self, num_sides, tesselates = False):
+        super().__init__(num_sides, tesselates)
+
+    def get_info(self):
+    #create a function to return shape info 
+        statement_1 = f"The number of sides is: {self.num_sides}\n"
+        if self.tesselates == True:
+            return statement_1 + "This shape tesselates!"
+        else: 
+            return statement_1 + "This shape does not tessealte!"
+       
+
+    def __str__(self):
+        return self.get_info()
+        
+
+
+if __name__ == "__main__": 
+    circle = Circle(100, False)
+    print(circle)
+    
+     
